930cb7b1b9cd9815e84ebe11f897ee2d388123a7
PlayBoiUzi <dzakyrafi123@gmail.com>
2025-03-13 11:05:12 +0700

    void display

--- Bubble_Sort.cpp
+++ Bubble_Sort.cpp
@@ -44,6 +44,19 @@ void bubbleSortArray(){
 
 }
 
+void display(){
+    cout << "=================================" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "=================================" << endl;
+    cout << endl;
+    for (int j=0;j<n;j++){
+        cout << arr[j];
+        if (j<n-1) {
+            cout << " --> ";
+        }
+    }
+}
+
 int main(){
 
 }
